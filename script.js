ce04479c60f77096719f8fc24f20e6ccd6853c5a
iRecycled <37885819+iRecycled@users.noreply.github.com>
2019-02-04 11:16:38 -0600

    sidebar

--- script.js
+++ script.js
@@ -2,6 +2,30 @@ function registerClick(){
     $('#registerModal').modal('show')
 }
 
-function registerClick(){
+function loginClick(){
     $('#loginModal').modal('show')
 }
+
+$('.collapse').show();
+
+$("#collapse").click(function(e) {
+    $(".collapse").collapse("hide");
+ });
+
+$(document).ready(function () {
+
+    $("#sidebar").mCustomScrollbar({
+         theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        // open or close navbar
+        $('#sidebar').toggleClass('active');
+        // close dropdowns
+        $('.collapse.in').toggleClass('in');
+        // and also adjust aria-expanded attributes we use for the open/closed arrows
+        // in our CSS
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+});
