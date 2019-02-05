9300831e387dd4eeb7ca77c266b1861abd39ac6a
Ryan Vanden Boomen <theonlyboomer@gmail.com>
2019-02-05 13:05:59 -0600

    Enhanced Chat Functionality

--- chat.js
+++ chat.js
@@ -4,28 +4,44 @@ function submitText(){
     text = parseText(text)
     text = generateMsg(text, "", "")
     chatOutputArea.innerHTML += text
+    resetScroll();
 }
 
 function parseText(text){
-    return text
+
+    return text;
 }
 
 function generateMsg(text, sender, time)
 {
+    var username = "Cool Guy"
+    var today = new Date();
     msg = "\
     <tr>\
-        <td>" + text + "\
+        <td style='vertical-align: top;'>\
+            <div class='message'>\
+            <p class ='msgUname'>"
+                + username
+                + "<span class='msgDate'>"
+                    + today.getMonth() + '-' +  today.getDay() + '-' + today.getFullYear() + ' ' + today.getHours() + ':' + ('0'+today.getMinutes()).slice(-2) + '</span>'
+            + "</p>"  
+            + text + "\
+            </div>\
         </td>\
     </tr>"
     return msg
 }
+
+function resetScroll(){
+    chatOutputBox.scrollTop =  chatOutputBox.scrollHeight - chatOutputBox.clientHeight;
+} 
 
 document.addEventListener("DOMContentLoaded", function(event) { 
     
     submitButton = document.getElementById("chatTextSubmit")
     chatInputArea = document.getElementById("chatTextArea")
     chatOutputArea = document.getElementById("chatBody")
-    
+    chatOutputBox = document.getElementById("chatOutput")
 
     
     submitButton.addEventListener("click", submitText)
