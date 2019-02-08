96adef3b1203750a855df2b2438487ef8442f7fe
Student <kauerinspace@gmail.com>
2019-02-08 13:46:24 -0600

    test3

--- register.php
+++ register.php
@@ -9,11 +9,12 @@ if ( isset( $_POST['submit'] ) ) {
 //$query = "INSERT INTO logins (id, username, password) 
  //       VALUES ('0','$_POST['username']', '$_POST['password']')";
   $username = $_POST['username']; 
+  $email = $_POST['email'];
   $password = $_POST['password'];
   
         // 2. Run the Query
         //$query = "INSERT INTO WebUser VALUES ((SELECT * FROM (SELECT COALESCE(MAX(UserId+1,0) FROM WebUser) as tmptable), 'Kevin', 'notmyactualpassword', 'jensenk2136@my.uwstout.edu'";
-        $query = "INSERT INTO WebUser (UserID, UserName, Password, email) VALUES (3, 'Kevin', 'notmyactualpassword', 'jensenk2136@my.uwstout.edu')";
+        $query = "INSERT INTO WebUser (UserID, UserName, Password, email) VALUES (4, '$username', '$password', '$email')";
 	$stmt = simpleQuery($db, $query);
         if($stmt == NULL) {
            include 'home.html';
