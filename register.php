316180cdc1d42e4c3c9eda0883452f505c9426c6
Student <kauerinspace@gmail.com>
2019-02-08 12:35:40 -0600

    this is a message

---
+++ register.php
@@ -0,0 +1,26 @@
+<?php
+	
+header("index.php");
+if ( isset( $_POST['submit'] ) ) {
+        // 1. Connect to the database
+        include "database.php";
+        $db = connectToDatabase(DBDeets::DB_NAME);
+        
+//$query = "INSERT INTO logins (id, username, password) 
+ //       VALUES ('0','$_POST['username']', '$_POST['password']')";
+  $username = $_POST['username']; 
+  $password = $_POST['password'];
+  
+        // 2. Run the Query
+        $query = "INSERT INTO logins (username, password) VALUES ('$username', '$password')";
+     
+	$stmt = simpleQuery($db, $query);
+        if($stmt == NULL) {
+           
+        }
+      	else{
+          
+          
+        }
+}
+?>
