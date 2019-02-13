41da61f9fa2affefabddc36d47860e0a8c9a5de2
Jensen <jensenk2136@uwstout.edu>
2019-02-13 11:43:21 -0600

    server adding

--- server.php
+++ server.php
@@ -6,14 +6,19 @@ if ( isset( $_POST['submit'] ) ) {
         include "database.php";
         $db = connectToDatabase(DBDeets::DB_NAME);
         
-  $servername = $_POST['servername']; 
+  $servername = $_POST['newServerName']; 
+  if($servername == NULL){
+        include 'register.html';
+  }
+  else {
         $query = "INSERT INTO Server VALUES ((SELECT * FROM (SELECT COALESCE(MAX(ServerId)+1,0) FROM Server) as tmptable), '$servername')";
 	$stmt = simpleQuery($db, $query);
         if($stmt == NULL) {
            include 'register.html';
         }
       	else{
-           include 'login.html';
+            include 'profile_page.html';
         }
+  }
 }
 ?>
