6f53ab2445e1ffa06959c21d254f0421e6232eef
Student <kauerinspace@gmail.com>
2019-02-13 09:28:04 -0600

    fixed failure endpoint

--- register.php
+++ register.php
@@ -18,7 +18,7 @@ if ( isset( $_POST['submit'] ) ) {
         $query = "INSERT INTO WebUser VALUES ((SELECT * FROM (SELECT COALESCE(MAX(UserId)+1,0) FROM WebUser) as tmptable), '$username', '$password', '$email')";
 	$stmt = simpleQuery($db, $query);
         if($stmt == NULL) {
-           include 'test.html';
+           include 'register.html';
         }
       	else{
             include 'login.html';
