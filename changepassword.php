57d303a0e9d0262255a61ff34431b22e9222a0af
Jensen <jensenk2136@uwstout.edu>
2019-02-15 12:21:35 -0600

    password

--- changepassword.php
+++ changepassword.php
@@ -1,30 +1,27 @@
 <?php
-header("index.php");
-        // 1. Connect to the database
         include "database.php";
         $db = connectToDatabase(DBDeets::DB_NAME);
         if ($db->connect_error) {
-            http_response_code(500);
-            die('{ "errMessage": "Failed to Connect to DB." }');
+                http_response_code(500);
+                die('{ "errMessage": "Failed to Connect to DB." }');
         }
-  $name = $_POST['username']; 
-  $oldpass = $_POST['oldPass'];
-  $newpass = $_POST['newPass'];
-  $checkpass = $_POST['confirmNewPass'];
-        // 2. Run the Query
-  //SELECT username, password, money FROM logins WHERE username = '$username';
-        $query = "SELECT UserID, UserName, Password, email FROM WebUser WHERE UserName = '$name';";
+  $username = $_POST['username']; 
+  $password = $_POST['password'];
+  $newPassword = $_POST['newPassword'];
+  $confirmNewPassword = $_POST['confirmNewPassword'];
+
+        $query = "SELECT Password FROM WebUser WHERE UserName = '$username';";
         $stmt = simpleQuery($db, $query);
   
         if($stmt == NULL) {
 
         				  }
  		 else{
-      		$stmt->bind_result($userID, $username, $password, $email);
+      		$stmt->bind_result($currentPassword);
         $stmt->fetch();
-  			if(strcmp($oldpass,$password)==0){
-              if(strcmp($newpass,$checkpass)==0){
-                $query2 = "UPDATE WebUser SET Password='$newpass' WHERE UserName = '$username';";
+  			if(strcmp($password,$currentPassword)==0){
+              if(strcmp($newPassword,$confirmNewPassword)==0){
+                $query2 = "UPDATE WebUser SET Password='$newPassword' WHERE UserName = '$username';";
                 $stmt2 = simpleQuery($db, $query2);
                 if($stmt2 == NULL) {
                     
