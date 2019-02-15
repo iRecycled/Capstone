dc49b032c2a85ef0769870f0b2b6f40686415e26
Jensen <jensenk2136@uwstout.edu>
2019-02-15 13:08:05 -0600

    server member

--- server.php
+++ server.php
@@ -11,7 +11,7 @@
             include 'register.html';
         }
         else {
-            $query = "SELECT COALESCE(MAX(ServerId+1,0) FROM Server";
+            $query = "SELECT * FROM (SELECT COALESCE(MAX(ServerID)+1,0) FROM Server) as tmptable";
             $stmt = simpleQuery($db, $query);
             $stmt->bind_result($serverId);
             //$query = "INSERT INTO Server VALUES ('$serverId', '$servername')";
