537a07c73d4c624e3c2fdf140a48d637f8112970
Student <kauerinspace@gmail.com>
2019-02-15 11:08:52 -0600

    vf

--- changePassword.php
+++ changePassword.php
@@ -1,37 +1,45 @@
 <?php
+header("index.php");
         // 1. Connect to the database
         include "database.php";
         $db = connectToDatabase(DBDeets::DB_NAME);
-        
-        $name = $_POST['username'];
-        $oldPass = $_POST['oldPass']; 
-        $newPass = $_POST['newPass'];
-        $confirmPass = $_POST['confirmNewPass';]
-  
+        if ($db->connect_error) {
+            http_response_code(500);
+            die('{ "errMessage": "Failed to Connect to DB." }');
+        }
+  $name = $_POST['username']; 
+  $oldpass = $_POST['oldPass'];
+  $newpass = $_POST['newPass'];
+  $checkpass = $_POST['confirmNewPass'];
         // 2. Run the Query
+  //SELECT username, password, money FROM logins WHERE username = '$username';
         $query = "SELECT UserID, UserName, Password, email FROM WebUser WHERE UserName = '$name';";
         $stmt = simpleQuery($db, $query);
+  
+        if($stmt == NULL) {
 
-    	$stmt->bind_result($userID, $username, $password, $email);
-		$stmt->fetch();
-  		if(strcmp($oldPass,$password)==0){
-  	        if(strcmp($newPass,$confirmPass)==0){
-                //insert new password into database with an update
-                //Query not set up yet
-                $query = "UPDATE WebUser SET Password='$newPass' WHERE UserName = '$name';";
-                $stmt = simpleQuery($db, $query);
-                if($stmt != NULL){
-                    //send alert that the change password was accepted
-                }               
-            }
-            else{
-                //display an error message in the modal
-                $error = "Your new passord must match";
-            }
-		}
-        else{
-            //display an error message in the modal
-            $error = "Your old password is incorrect"
-        }
-    echo json_encode($error);
+        				  }
+ 		 else{
+      		$stmt->bind_result($userID, $username, $password, $email);
+        $stmt->fetch();
+  			if(strcmp($oldpass,$password)==0){
+              if(strcmp($newpass,$checkpass)==0){
+                $query2 = "UPDATE WebUser SET Password='$newpass' WHERE UserName = '$username';";
+                $stmt2 = simpleQuery($db, $query2);
+                if($stmt2 == NULL) {
+                    
+                                  }
+                  else{
+                      
+                  }
+              }
+   			 }
+           else{
+             include "register.html";
+           }
+         }
+  }
+  else {
+    include "login.html";
+  }
 ?>
