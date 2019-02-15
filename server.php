914f5213029ce0972ae8f968e67419762c3030c8
Jensen <jensenk2136@uwstout.edu>
2019-02-15 12:51:13 -0600

    server member

--- server.php
+++ server.php
@@ -12,9 +12,11 @@
         }
         else {
             $query = "SELECT COALESCE(MAX(ServerId),0) FROM Server";
-            $serverId = simpleQuery($db, $query);
-            $query = "INSERT INTO Server VALUES ('$serverId', '$servername')";
-	      $stmt = simpleQuery($db, $query);
+            $stmt = simpleQuery($db, $query);
+            $stmt->bind_result($serverId);
+            echo json_encode($serverId);
+            //$query = "INSERT INTO Server VALUES ('$serverId', '$servername')";
+	      //$stmt = simpleQuery($db, $query);
             //$query = "SELECT UserId FROM WebUser WHERE UserName = '$username'";
             //$userId = simpleQuery($db, $query);
             //$query = "INSERT INTO ServerMember VALUES ('$serverId', '$userId'";
