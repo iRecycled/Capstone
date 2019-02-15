6ccdb0556d9d5efb765e5cbd2c71234925f91e18
Jensen <jensenk2136@uwstout.edu>
2019-02-15 11:59:32 -0600

    password

--- changepassword.php
+++ changepassword.php
@@ -4,39 +4,38 @@ header("index.php");
         include "database.php";
         $db = connectToDatabase(DBDeets::DB_NAME);
         if ($db->connect_error) {
+            http_response_code(500);
             die('{ "errMessage": "Failed to Connect to DB." }');
         }
-  $name = $_POST['username']; 
-  $oldpass = $_POST['oldPass'];
-  $newpass = $_POST['newPass'];
-  $checkpass = $_POST['confirmNewPass'];
-        // 2. Run the Query
-  //SELECT username, password, money FROM logins WHERE username = '$username';
-        $query = "SELECT UserID, UserName, Password, email FROM WebUser WHERE UserName = '$name';";
-        $stmt = simpleQuery($db, $query);
-  
-        if($stmt == NULL) {
+  $username = $_POST['username']; 
+  $password = $_POST['password'];
+  $newPassword = $_POST['newPassword'];
+  $confirmPassword = $_POST['confirmPassword'];
 
-        				  }
- 		 else{
-      		$stmt->bind_result($userID, $username, $password, $email);
-        $stmt->fetch();
-  			if(strcmp($oldpass,$password)==0){
-              if(strcmp($newpass,$checkpass)==0){
-                $query2 = "UPDATE WebUser SET Password='$newpass' WHERE UserName = '$username';";
-                $stmt2 = simpleQuery($db, $query2);
-                if($stmt2 == NULL) {
-                    
-                                  }
+        $query = "SELECT Password FROM logins WHERE username = '$username';"
+        $stmt = simpleQuery($db, $query);
+
+            if($stmt == NULL) {
+            }
+ 	      else{
+      		$stmt->bind_result($currentPassword);
+                  $stmt->fetch();
+  			if(strcmp($currentPassword, $password)==0){
+                        if(strcmp($newPassword,$confirmPassword)==0){
+                              $query2 = "UPDATE WebUser SET Password='$newPassword' WHERE UserName = '$username';";
+                              $stmt2 = simpleQuery($db, $query2);
+                              if($stmt2 == NULL) {
+
+                              }
+                              else{
+
+                              }
+                        }
+   			}
                   else{
-                      
+                        include "register.html";
                   }
-              }
-   			 }
-           else{
-             include "register.html";
-           }
-         }
+            }
   }
   else {
     include "login.html";
