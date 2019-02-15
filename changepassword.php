d29124a4b319378bffe42cfd378eb7ca25f825f1
Jensen <jensenk2136@uwstout.edu>
2019-02-15 12:26:07 -0600

    formatting

--- changepassword.php
+++ changepassword.php
@@ -1,42 +1,32 @@
 <?php
-        include "database.php";
-        $db = connectToDatabase(DBDeets::DB_NAME);
-        if ($db->connect_error) {
-                http_response_code(500);
-                die('{ "errMessage": "Failed to Connect to DB." }');
-        }
-  $username = $_POST['username']; 
-  $password = $_POST['password'];
-  $newPassword = $_POST['newPassword'];
-  $confirmNewPassword = $_POST['confirmNewPassword'];
+      include "database.php";
+      $db = connectToDatabase(DBDeets::DB_NAME);
+      if ($db->connect_error) {
+            http_response_code(500);
+            die('{ "errMessage": "Failed to Connect to DB." }');
+      }
+      $username = $_POST['username']; 
+      $password = $_POST['password'];
+      $newPassword = $_POST['newPassword'];
+      $confirmNewPassword = $_POST['confirmNewPassword'];
 
-        $query = "SELECT Password FROM WebUser WHERE UserName = '$username';";
-        $stmt = simpleQuery($db, $query);
+      $query = "SELECT Password FROM WebUser WHERE UserName = '$username';";
+      $stmt = simpleQuery($db, $query);
   
-        if($stmt == NULL) {
-
-        				  }
- 		 else{
-      		$stmt->bind_result($currentPassword);
-        $stmt->fetch();
-  			if(strcmp($password,$currentPassword)==0){
-              if(strcmp($newPassword,$confirmNewPassword)==0){
-                $query2 = "UPDATE WebUser SET Password='$newPassword' WHERE UserName = '$username';";
-                $stmt2 = simpleQuery($db, $query2);
-                if($stmt2 == NULL) {
-                    
-                                  }
-                  else{
-                      
+      if($stmt != NULL) {
+      	$stmt->bind_result($currentPassword);
+            $stmt->fetch();
+  		if(strcmp($password,$currentPassword)==0){
+                  if(strcmp($newPassword,$confirmNewPassword)==0){
+                        $query2 = "UPDATE WebUser SET Password='$newPassword' WHERE UserName = '$username';";
+                        $stmt2 = simpleQuery($db, $query2);
                   }
-              }
-   			 }
-           else{
-             include "register.html";
-           }
-         }
-  }
-  else {
-    include "login.html";
-  }
+   		}
+            else{
+                  include "register.html";
+            }
+      }
+      else {
+            include "login.html";
+      }
 ?>
