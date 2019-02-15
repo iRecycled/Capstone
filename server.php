f930e65bf762328ed855a44831c68b15f02a8ecb
Jensen <jensenk2136@uwstout.edu>
2019-02-15 12:35:07 -0600

    servermembers changes

--- server.php
+++ server.php
@@ -5,18 +5,26 @@
                 http_response_code(500);
                 die('{ "errMessage": "Failed to Connect to DB." }');
             }
-  $servername = $_POST['servername']; 
-  if($servername == NULL){
-        include 'register.html';
-  }
-  else {
-        $query = "INSERT INTO Server VALUES ((SELECT * FROM (SELECT COALESCE(MAX(ServerId)+1,0) FROM Server) as tmptable), '$servername')";
-	$stmt = simpleQuery($db, $query);
-        if($stmt == NULL) {
-           include 'register.html';
+        $username = $_POST['username'];
+        $servername = $_POST['servername']; 
+        if($servername == NULL){
+            include 'register.html';
         }
+        else {
+            $query = "INSERT INTO Server VALUES ((SELECT * FROM (SELECT COALESCE(MAX(ServerId)+1,0) FROM Server) as tmptable), '$servername')";
+	      $stmt = simpleQuery($db, $query);
+            if($stmt == NULL) {
+                  include 'register.html';
+            }
       	else{
-            include 'profile_page.html';
+                  include 'profile_page.html';
+            }
+            $query = "INSERT INTO ServerMember VALUES ((SELECT * FROM (SELECT COALESCE(MAX(ServerId),0) FROM Server) as tmptable), (SELECT UserId FROM WebUser WHERE UserName = '$username')";
+            if($stmt == NULL) {
+                  include 'register.html';
+            }
+      	else{
+                  include 'profile_page.html';
+            }
         }
-  }
 ?>
