dfa68295f685afcf74640c24d4e0ae8bc04e66d2
Jensen <jensenk2136@uwstout.edu>
2019-02-18 12:45:27 -0600

    getServerList

--- getServerList.php
+++ getServerList.php
@@ -6,7 +6,7 @@
                 die('{ "errMessage": "Failed to Connect to DB." }');
             }
         $username = $_POST['username']; 
-        $query = "SELECT ServerName FROM Server s JOIN ServerMember sm ON s.ServerId = sm.ServerId JOIN WebUser wu ON wu.UserId = sm.UserId";
+        $query = "SELECT ServerName FROM Server s JOIN ServerMember sm ON s.ServerId = sm.ServerId JOIN WebUser wu ON wu.UserId = sm.UserId WHERE wu.UserName = '$username';";
         $result = $db->query($query);
         $response = array();
         while($row = $result->fetch_array(MYSQLI_ASSOC)) {
