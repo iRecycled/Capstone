be0fc20e740016b3d28c36d3efa7c3483857cf64
emiller7231 <34141292+emiller7231@users.noreply.github.com>
2019-02-18 13:05:17 -0600

    Merge ethanTest process.php
    Last merge

--- process.php
+++ process.php
@@ -1,5 +1,4 @@
 <?php
-
     $function = $_POST['function'];
     
     $log = array();
@@ -8,7 +7,7 @@
     
     	 case('getState'):
         	 if(file_exists('chat.txt')){
-               $lines = file('http://144.13.22.61/CS458SP19/Team1/Capstone/chat.txt');
+               $lines = file('chat.txt');
         	 }
              $log['state'] = count($lines); 
         	 break;	
@@ -16,7 +15,7 @@
     	 case('update'):
         	$state = $_POST['state'];
         	if(file_exists('chat.txt')){
-        	   $lines = file('http://144.13.22.61/CS458SP19/Team1/Capstone/chat.txt');
+        	   $lines = file('chat.txt');
         	 }
         	 $count =  count($lines);
         	 if($state == $count){
@@ -41,21 +40,16 @@
     	 
     	 case('send'):
 		  $nickname = htmlentities(strip_tags($_POST['nickname']));
-			 $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
-			  $message = htmlentities(strip_tags($_POST['message']));
-		 if(($message) != "\n"){
-        	
-			 if(preg_match($reg_exUrl, $message, $url)) {
-       			$message = preg_replace($reg_exUrl, '<a href="'.$url[0].'" target="_blank">'.$url[0].'</a>', $message);
-				} 
-			 
-        	
-        	 fwrite(fopen('http://144.13.22.61/CS458SP19/Team1/Capstone/chat.txt', 'a'), "<span>". $nickname . "</span>" . $message = str_replace("\n", " ", $message) . "\n"); 
-		 }
-        	 break;
-    	
+	     $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
+	     $message = htmlentities(strip_tags($_POST['message']));
+	     if (($message) != "\n") {
+	       if (preg_match($reg_exUrl, $message, $url)) {
+	          $message = preg_replace($reg_exUrl, '<a href="'.$url[0].'" target="_blank">'.$url[0].'</a>', $message);
+	       } 
+	          fwrite(fopen('chat.txt', 'a'), $message = str_replace("\n", " ", $message) . "\n"); 
+	     }
+         break;
     }
     
     echo json_encode($log);
-
 ?>
