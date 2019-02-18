7ecad46c5406cc1c9d8e74fe2117001bd777da8a
Jensen <jensenk2136@uwstout.edu>
2019-02-18 11:29:16 -0600

    password

--- register.php
+++ register.php
@@ -6,34 +6,28 @@ if ( isset( $_POST['submit'] ) ) {
         include "database.php";
         $db = connectToDatabase(DBDeets::DB_NAME);
         
-//$query = "INSERT INTO logins (id, username, password) 
- //       VALUES ('0','$_POST['username']', '$_POST['password']')";
-  $username = $_POST['username']; 
-  $email = $_POST['email'];
-  $password = $_POST['password'];
-<<<<<<< HEAD
-=======
+        //$query = "INSERT INTO logins (id, username, password) 
+        //       VALUES ('0','$_POST['username']', '$_POST['password']')";
+        $username = $_POST['username']; 
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
-  if(($username==NULL)||($email==NULL)||($password==NULL)){
-        include 'register.html';
-  }
-  else{
->>>>>>> 690314d2bccdeb57a2a4872f52cf75a653056be2
-        //INSERT INTO WebUser VALUES ((SELECT * FROM (SELECT COALESCE(MAX(UserId)+1,0) FROM WebUser) as tmptable), 'Kevin', 'notmyactualpassword', 'jensenk2136@my.uwstout.edu')
-        // 2. Run the Query
-        //$query = "INSERT INTO WebUser (UserName, Password, email) VALUES ('$username', '$password', '$email')";
-        //$query = "INSERT INTO WebUser VALUES ((SELECT * FROM (SELECT COALESCE(MAX(UserId+1,0) FROM WebUser) as tmptable), 'Kevin', 'notmyactualpassword', 'jensenk2136@my.uwstout.edu'";
-        $query = "INSERT INTO WebUser VALUES ((SELECT * FROM (SELECT COALESCE(MAX(UserId)+1,0) FROM WebUser) as tmptable), '$username', '$password', '$email')";
-	$stmt = simpleQuery($db, $query);
-        if($stmt == NULL) {
-           include 'register.html';
+        if(($username==NULL)||($email==NULL)||($password==NULL)){
+                include 'register.html';
         }
-      	else{
-            include 'login.html';
+        else{
+                //INSERT INTO WebUser VALUES ((SELECT * FROM (SELECT COALESCE(MAX(UserId)+1,0) FROM WebUser) as tmptable), 'Kevin', 'notmyactualpassword', 'jensenk2136@my.uwstout.edu')
+                // 2. Run the Query
+                //$query = "INSERT INTO WebUser (UserName, Password, email) VALUES ('$username', '$password', '$email')";
+                //$query = "INSERT INTO WebUser VALUES ((SELECT * FROM (SELECT COALESCE(MAX(UserId+1,0) FROM WebUser) as tmptable), 'Kevin', 'notmyactualpassword', 'jensenk2136@my.uwstout.edu'";
+                $query = "INSERT INTO WebUser VALUES ((SELECT * FROM (SELECT COALESCE(MAX(UserId)+1,0) FROM WebUser) as tmptable), '$username', '$password', '$email')";
+	        $stmt = simpleQuery($db, $query);
+                if($stmt == NULL) {
+                        include 'register.html';
+                }
+      	        else{
+                        include 'login.html';
+                }
         }
 }
-<<<<<<< HEAD
-=======
-}
->>>>>>> 690314d2bccdeb57a2a4872f52cf75a653056be2
 ?>
