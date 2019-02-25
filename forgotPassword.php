efae745c125d627e86072c5b006fa2714f43b161
jakaszu <45398626+jakaszu@users.noreply.github.com>
2019-02-25 12:58:40 -0600

    forgot password

---
+++ forgotPassword.php
@@ -0,0 +1,35 @@
+<?php
+    include "database.php";
+    $db = connectToDatabase(DBDeets::DB_NAME);
+    //connects to the database
+    if ($db->connect_error) {
+          http_response_code(500);
+          die('{ "errMessage": "Failed to Connect to DB." }');
+    }
+
+    $username = $_POST['username']; 
+    //randomly set password for the user to change
+    $password = "random characters";
+
+    $query = "SELECT email FROM WebUser WHERE UserName = '$username';";
+    $stmt = simpleQuery($db, $query);
+
+    $stmt->bind_result($email);
+    $stmt->fetch();
+    
+    $query = "UPDATE WebUser SET Password='$password' WHERE UserName = '$username';";
+    //runs query on the database
+    $stmt = simpleQuery($db, $query);
+
+    $msg = "Hi " . $username . ",\n\n Here is your new password:\n\n" . $password . 
+        "\nPlease login and change your password to something you will remember immediately.";
+    $subject = "TerryChat Password Reset"
+    //send email to the user with the new password
+    //mail('eamil','subject','message')
+    mail($email,$subject,$msg);
+    //alert box to be created and displayed
+    //<div class="alert alert-info alert-dismissible">
+        //<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+            //text
+    //</div>    
+?>
