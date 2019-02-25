8ad33ac1d9957dd067dc94ee9288a1ecf0a1546b
Ryan Vanden Boomen <theonlyboomer@gmail.com>
2019-02-25 12:31:12 -0600

    Server Titles

--- home.js
+++ home.js
@@ -5,10 +5,23 @@ $(document).ready(function(){
         data: {username: localStorage.getItem('username')},
         success: function(data) {
             obj = JSON.parse(data);
-            console.log(obj);
+            createServerTable(obj, "userServersBody")
         },
         error: function(data) {
             console.log("fail");
         }
     })
 });
+
+function createServerTable(d, targetID){
+    text = "";
+    for(i = 0; i < d.length(); i++)
+    {
+        text +="<tr>\
+                    <td>\
+                        " + d[i].ServerName + "\
+                    </td>\
+                <tr>";
+    }
+    document.getElementById(targetID).innerHTML += text;
+}
