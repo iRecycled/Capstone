36be62871a1b5c01afab45484adfdf44eaba4d50
jakaszu <45398626+jakaszu@users.noreply.github.com>
2019-02-27 12:10:45 -0600

    forgotpwd

--- forgotPassword.php
+++ forgotPassword.php
@@ -21,14 +21,12 @@
     //runs query on the database
     $stmt = simpleQuery($db, $query);
 
-    echo json_encode($email);
-    
     $msg = "Hi " . $username . ",\n\n Here is your new password:\n\n" . $password . 
         "\nPlease login and change your password to something you will remember immediately.";
     $subject = "TerryChat Password Reset"
     //send email to the user with the new password
     //mail('eamil','subject','message')
-    mail($email,$subject,$msg);
+    mail("kaszubskij3381@my.uwstout.edu",$subject,$msg);
     
     //alert box to be created and displayed
     //<div class="alert alert-info alert-dismissible">
