9c630846d0fb76b22c2b5d332723f471533522f6
Ryan Vanden Boomen <theonlyboomer@gmail.com>
2019-02-27 12:46:01 -0600

    Friends Query

--- getFriendsList.php
+++ getFriendsList.php
@@ -9,8 +9,7 @@
         $username = $_POST['username']; 
         $query = "SELECT UserID FROM WebUser WHERE UserName = '$username';";
         //runs the query
-        $response = array();
-        /*$stmt = simpleQuery($db, $query);
+        $stmt = simpleQuery($db, $query);
         $stmt->bind_result)($userID);
         $stmt->fetch();
         
@@ -20,7 +19,7 @@
         while($row = $result->fetch_array(MYSQLI_ASSOC)) {
             $response[] = $row;
         }
-        
+        /*
         $query = "SELECT UserName FROM Friend JOIN WebUser w1 ON Friend.Friend1ID = w1.UserID WHERE Friend2ID = '$userID';";
         $result = $db->query($query);
         $response = array();
