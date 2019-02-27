cf5bf34f5beaeb628c33d2583f36fdd5cced8761
fergesen <45017714+fergesen@users.noreply.github.com>
2019-02-27 12:53:38 -0600

    Update server_settings.js

--- server_settings.js
+++ server_settings.js
@@ -6,10 +6,10 @@ $(document).ready(function(){
         success: function(data) {
             console.log(data);
             obj = JSON.parse(data);
-            //console.log(obj);
-            document.getElementById("servername").innerHTML = data.serverName;
+            console.log(obj);
+            document.getElementById("servername").innerHTML = obj.serverName;
         },
-        error: function(data) {
+        error: function() {
             console.log("fail");
         }
     })
