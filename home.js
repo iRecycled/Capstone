70ea3c91b5979da57ff16891ac7c4980845a59aa
Ryan Vanden Boomen <theonlyboomer@gmail.com>
2019-03-04 12:59:18 -0600

    Dynamic Profile Page

--- home.js
+++ home.js
@@ -63,7 +63,7 @@ function createFriendsList(d, targetID){
     {
         text +="<tr>\
                     <td class = 'serverEntry'>\
-                        <a  onclick = 'setViewInfo('"+names[i]+"')'>\
+                        <a  onclick = 'setViewInfo(\""+names[i]+"\")'>\
                         " + names[i] + "\
                         </a>\
                     </td>\
