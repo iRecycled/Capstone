ec4cfbc663bffe85e085e4ef54de9b0bf904de07
Student <kauerinspace@gmail.com>
2019-03-04 09:28:04 -0600

    wef

--- register.php
+++ register.php
@@ -1,17 +1,15 @@
 <?php
+header("index.php");
         // 1. Connect to the database
         include "database.php";
         $db = connectToDatabase(DBDeets::DB_NAME);
         if ($db->connect_error) {
-                http_response_code(500);
-                die('{ "errMessage": "Failed to Connect to DB." }');
-            }
-
-
+            http_response_code(500);
+            die('{ "errMessage": "Failed to Connect to DB." }');
+        }
   $username = $_POST['username']; 
   $email = $_POST['email'];
   $password = $_POST['password'];
-  $data=0;
         //INSERT INTO WebUser VALUES ((SELECT * FROM (SELECT COALESCE(MAX(UserId)+1,0) FROM WebUser) as tmptable), 'Kevin', 'notmyactualpassword', 'jensenk2136@my.uwstout.edu')
         // 2. Run the Query
         //$query = "INSERT INTO WebUser (UserName, Password, email) VALUES ('$username', '$password', '$email')";
