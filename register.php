e9f34ef1074f1bdde7b6d7516e01172164a3d79b
Student <kauerinspace@gmail.com>
2019-03-04 08:40:49 -0600

    adding db connect error

--- register.php
+++ register.php
@@ -2,7 +2,12 @@
         // 1. Connect to the database
         include "database.php";
         $db = connectToDatabase(DBDeets::DB_NAME);
-        
+        if ($db->connect_error) {
+                http_response_code(500);
+                die('{ "errMessage": "Failed to Connect to DB." }');
+            }
+
+
   $username = $_POST['username']; 
   $email = $_POST['email'];
   $password = $_POST['password'];
