7551844a4cf503ee635b89c267520db861514603
jakaszu <45398626+jakaszu@users.noreply.github.com>
2019-03-06 12:02:35 -0600

    try

--- changePassword.php
+++ changePassword.php
@@ -26,14 +26,14 @@
                   if(strcmp($newPassword,$confirmNewPassword)==0){
                         $query2 = "UPDATE WebUser SET Password='$newPassword' WHERE UserName = '$username';";
                         $stmt2 = simpleQuery($db, $query2);
-                        die('{"msg": "password changed successfully!');
+                        echo '{"msg": "password changed successfully!';
                   }
                   else{
-                        die('{"msg": "failed to change password"}');
+                        echo '{"msg": "failed to change password"}';
                   }
    		}
             else{
-                  die('{"msg": "failed to change password"}');
+                  echo '{"msg": "failed to change password"}';
             }
       }
 ?>
