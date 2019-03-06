73154f65267eb4ca07b5e1a1659935f728fb0c63
iRecycled <37885819+iRecycled@users.noreply.github.com>
2019-03-06 11:07:53 -0600

    register

--- register.php
+++ register.php
@@ -10,7 +10,28 @@ header("index.php");
   $username = strip_tags($_POST['username']); 
   $email = strip_tags($_POST['email']);
   $password = strip_tags($_POST['password']);
+
         // 2. Run the Query
+        $emailCheck = "SELECT * FROM WebUser WHERE username='$username'";
+        $mailResult = simpleQuery($db,$emailCheck);
+
+        $usernameCheck = "SELECT * FROM WebUser WHERE password='$password'";
+        $userResult = simpleQuery($db,$emailCheck);
+
+        if($mailResult == NULL){
+                $data = -2; //email already in use
+        }
+        else if($userResult == NULL)
+        {
+                $data = -3;//username already in use      
+        }
+        else { //if username and password dont exist then create user
+                $query = "INSERT INTO WebUser VALUES ((SELECT * FROM (SELECT COALESCE(MAX(UserId)+1,0) FROM WebUser) as tmptable), '$username', '$password', '$email')";
+	        $stmt = simpleQuery($db, $query);
+        }
+
+        
+
         $query = "INSERT INTO WebUser VALUES ((SELECT * FROM (SELECT COALESCE(MAX(UserId)+1,0) FROM WebUser) as tmptable), '$username', '$password', '$email')";
 	$stmt = simpleQuery($db, $query);
         if($stmt != NULL) {
