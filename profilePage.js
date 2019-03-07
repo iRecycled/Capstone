dbb9a951cd97ff8d1f96d9db83ca6a56ada873f1
jakaszu <45398626+jakaszu@users.noreply.github.com>
2019-03-07 11:39:46 -0600

    afew

--- profilePage.js
+++ profilePage.js
@@ -46,7 +46,7 @@ $(document).ready(function(){
             window.location.href = 'http://144.13.22.61/CS458SP19/Team1/Capstone/home.html';
             console.log(result);
             if(result == 0){
-                alert("Password failed to change.");
+                alert(result);
             }
             else{
                 alert("Password changed successfully!");
