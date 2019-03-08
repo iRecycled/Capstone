dd6bbeadd152966bb711d3806175c0e3826e4997
Kevin Jensen <jensenk2136@my.uwstout.edu>
2019-03-08 12:02:11 -0600

    test

--- sendFriendRequest.php
+++ sendFriendRequest.php
@@ -33,9 +33,9 @@ header("index.php");
                 $alreadyExists = 1;
         }
 
-        /*if($alreadyExists == 0) {
+        if($alreadyExists === 0) {
                 echo($userIDFrom, $userIDTo);
                 $query = "INSERT INTO FriendRequest VALUES ('$userIDFrom', '$userIDTo');";
                 $stmt = simpleQuery($db, $query);
-        }*/
+        }
 ?>
