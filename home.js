985da5a142f1b94f4472f8ab893384e245fbcd1a
emiller7231 <34141292+emiller7231@users.noreply.github.com>
2019-03-08 12:45:25 -0600

    Revert "Merge branch 'master' of https://github.com/iRecycled/Capstone"
    This reverts commit d7b47bc3a48a6459b9cc5055f8c86f198492966f, reversing
    changes made to c4daa7ebd2ef1535448c065ffbbdb024f3dad0d8.

--- home.js
+++ home.js
@@ -167,11 +167,3 @@ function sendServerRequest(id){
     })
     */
 }
-
-$('html').click(function(e) {                    
-    if(!$(e.target).id == 'searchButton' && !$(e.target).hasClass('dropdown-item'))
-    {
-        console.log("out click")
-        $('#searchDropdown').classList.toggle("show");                
-    }
- }); 
