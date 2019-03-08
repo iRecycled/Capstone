c764fa1e8523e327b93b2b69542a2571a7e08be2
Kevin Jensen <jensenk2136@my.uwstout.edu>
2019-03-08 09:34:35 -0600

    test

--- profilePage.js
+++ profilePage.js
@@ -151,7 +151,7 @@ function SendFriendRequest()
     user = localStorage.getItem('username');
     friend = localStorage.getItem('viewInfo');
     //SEND REQUEST
-    alert("Sending request");
+    console.log("TEST");
     return $.ajax({
         type: "post",
         url: "sendFriendRequest.php",
