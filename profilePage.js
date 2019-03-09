f16d7b0022e03ea56519c403d9df972f9b52e060
Ryan Vanden Boomen <theonlyboomer@gmail.com>
2019-03-08 18:19:29 -0600

    Minor tweaks

--- profilePage.js
+++ profilePage.js
@@ -138,13 +138,15 @@ $(document).ready(function(){
 var isNotFriend = false;
 function AddFriendButton(e){
     friendName = localStorage.getItem('viewInfo')
-    CheckFriend(friendName)
-    if((localStorage.getItem('username') != friendName) && isNotFriend)
-    {
-        console.log("Calls");
-        document.getElementById(e).innerHTML += "<h4><a><u>Add Friend</u></a></h4>";
-        document.getElementById(e).onclick = function(){ SendFriendRequest()};
-    }
+    $.when(CheckFriend(friendName)).done(function(a1){
+        if((localStorage.getItem('username') != friendName) && isNotFriend)
+        {
+            console.log("Calls");
+            document.getElementById(e).innerHTML += "<h4><a><u>Add Friend</u></a></h4>";
+            document.getElementById(e).onclick = function(){ SendFriendRequest()};
+        }
+    });
+
 }
 
 function SendFriendRequest()
