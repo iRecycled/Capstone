fc1f8ae19c63552d73ada8095069a7fc3dc4efe4
Student <kauerinspace@gmail.com>
2019-03-11 11:38:13 -0500

    test

--- login.php
+++ login.php
@@ -20,9 +20,14 @@ header("index.php");
   
       	$stmt->bind_result($userID, $username, $password, $email, $sessionID);
         $stmt->fetch();
+
+        $query = "SELECT Password FROM WebUser WHERE UserName = '$name';";
+        $stmt = simpleQuery($db, $query);
+        $hashBool=password_verify($password, $stmt->get_result());
+
         $bool1 = strcmp($name,$username)==0;
         $bool2 = strcmp($pass,$password)==0;
-  			if($bool1 && $bool2){
+  			if($bool1 && $hashBool){
           $rand=rand(1, 50000);
           $query = "UPDATE WebUser SET SessionID=$rand WHERE UserName = '$username';";
           $stmt = simpleQuery($db, $query);
