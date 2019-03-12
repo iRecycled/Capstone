78c75b5309aa795f7740aa6898eca06b68869f81
Kauer <kauera6146@uwstout.edu>
2019-03-11 23:38:07 -0500

    ohey

--- login.php
+++ login.php
@@ -36,8 +36,8 @@ header("index.php");
             if (mysqli_num_rows($result) > 0) {
                 while($row = mysqli_fetch_assoc($result)) {
                   
-                  //password_verify($_POST['pass'],$row['Password'])
-                    if(strlen($row['Password'])>strlen(password_hash($_POST['pass']), PASSWORD_DEFAULT)){
+                  $bool=password_verify($_POST['pass'],$row['Password'])
+                    if($bool==1){
                         return true;
                     }
                 }
@@ -83,13 +83,13 @@ header("index.php");
         }
 
   $name = strip_tags($_POST['user']);
-  $pass = strip_tags($_POST['pass']);
+  //$pass = strip_tags($_POST['pass']);
 
   //$query = "SELECT Password FROM WebUser WHERE UserName = '$name';";
   //$stmt2 = simpleQueryPassword($db, $query);
   
-  $query = "alter table WebUser modify column Password varchar(500) not null;";
-  $stmt = simpleQuery($db, $query);
+  //$query = "alter table WebUser modify column Password varchar(500) not null;";
+  //$stmt = simpleQuery($db, $query);
 
         $query = "SELECT UserID, UserName, Password, email, SessionID FROM WebUser WHERE UserName = '$name';";
         $stmt = simpleQuery($db, $query);
@@ -97,13 +97,13 @@ header("index.php");
       	$stmt->bind_result($userID, $username, $password, $email, $sessionID);
         $stmt->fetch();
 
-        //$query = "SELECT Password FROM WebUser WHERE UserName = '$name';";
-        //$stmt2 = simpleQueryPassword($db, $query);
+        $query = "SELECT Password FROM WebUser WHERE UserName = '$name';";
+        $stmt2 = simpleQueryPassword($db, $query);
         
         $bool1 = strcmp($name,$username)==0;
-        $bool2 = strcmp($pass,$password)==0;
+        //$bool2 = strcmp($pass,$password)==0;
         //$bool1 && $bool2
-  			if($bool1 && $bool2){
+  			if($bool1 && $stmt2){
           $rand=rand(1, 50000);
           $query = "UPDATE WebUser SET SessionID=$rand WHERE UserName = '$username';";
           $stmt = simpleQuery($db, $query);
