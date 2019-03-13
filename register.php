99bd3a8f376d49c79f1856f9d8ae57b7232ee818
Peterson <petersonn3916@uwstout.edu>
2019-03-13 12:46:38 -0500

    reg is dumb

--- register.php
+++ register.php
@@ -8,10 +8,10 @@ header("index.php");
             die('{ "errMessage": "Failed to Connect to DB." }');
         }
         $data = 0;
-  $username = strip_tags($_POST['username']); 
-  $email = strip_tags($_POST['email']);
-  $password = strip_tags($_POST['password']);
-        $check = 0;
+        $username = strip_tags($_POST['username']); 
+        $email = strip_tags($_POST['email']);
+        $password = strip_tags($_POST['password']);
+        $checkEmail = 0;
         // 2. Run the Query
         $query = "SELECT UserID, UserName, Password, email, SessionID FROM WebUser WHERE email = '$email';";
         $stmt = simpleQuery($db, $query);
@@ -21,7 +21,7 @@ header("index.php");
 
         if($emailTest == $email){
                 $data = 0; //email is already taken
-                $check = -2;
+                $checkEmail = -2;
         }
         
         $query = "SELECT UserID, UserName, Password, email, SessionID FROM WebUser WHERE UserName = '$username';";
@@ -32,7 +32,7 @@ header("index.php");
 
         if($usernameTest == $username){
                 $data = -3; //username is already taken
-                if($check == -2){
+                if($checkEmail == -2){
                         $data = -5;
                 }
         }
