dd472159ef50bc683b856f0d2cd98e060008fb69
jakaszu <45398626+jakaszu@users.noreply.github.com>
2019-03-13 12:32:27 -0500

    fixed forgotPassword message

--- forgotPassword.php
+++ forgotPassword.php
@@ -39,7 +39,7 @@
                 //send email to the user with the new password
                 mail($email,$subject,$msg);
                 // sends back 1 if email sent
-                $status = -1;
+                $status = 1;
                 break;
             }
             else {
@@ -66,7 +66,7 @@
         $subject = "TerryChat Password Reset";
         //send email to the user with the new password
         mail($email,$subject,$msg);
-        $status = -1;
+        $status = 1;
     }
     echo json_encode($status);
 ?>
