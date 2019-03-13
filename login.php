eac19b7a50bb57e5b06d8d4868e358b5dde0b84d
Student <kauerinspace@gmail.com>
2019-03-13 08:06:25 -0500

    ipn

--- login.php
+++ login.php
@@ -8,8 +8,8 @@ header("index.php");
             die('{ "errMessage": "Failed to Connect to DB." }');
         }
 
-  $name = strip_tags($_POST['user']);
-  $pass = strip_tags($_POST['pass']);
+  $name = mysql_real_escape_string($_POST['user']);
+  $pass = mysql_real_escape_string($_POST['pass']);
 
   //$query = "SELECT Password FROM WebUser WHERE UserName = '$name';";
   //$stmt2 = simpleQueryPassword($db, $query);
