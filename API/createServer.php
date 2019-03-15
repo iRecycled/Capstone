e7dd62d533547a90b858ee324cf36ca77822b1b6
Student <kauerinspace@gmail.com>
2019-03-15 11:57:11 -0500

    test

---
+++ API/createServer.php
@@ -0,0 +1,41 @@
+<?php
+        include "database.php";
+        $db = connectToDatabase(DBDeets::DB_NAME);
+        if ($db->connect_error) {
+                http_response_code(500);
+                die('{ "errMessage": "Failed to Connect to DB." }');
+            }
+
+            $data = json_decode(file_get_contents("php://input"));
+            $username = $data->username;
+            $servername = $data->servername;
+        if($servername == NULL){
+            include 'register.html';
+        } else {
+            //gets the highest server ID + 1
+            $query = "SELECT * FROM (SELECT COALESCE(MAX(ServerID)+1,0) FROM Server) as tmptable";
+            $stmt = simpleQuery($db, $query);
+            $stmt->bind_result($serverId);
+            $stmt->fetch();
+            //Creates the server in the database
+            $query = "INSERT INTO Server VALUES ('$serverId', '$servername')";
+            $stmt = simpleQuery($db, $query);
+            //Fetches the userId for the user that's logged in
+            $query = "SELECT UserId FROM WebUser WHERE UserName = '$username'";
+            $stmt = simpleQuery($db, $query);
+            $stmt->bind_result($userId);
+            $stmt->fetch();
+            //Assigns the user to the leader of the server
+            $query = "INSERT INTO ServerMember VALUES ('$serverId', '$userId', 0)";
+            $stmt = simpleQuery($db, $query);
+
+            //Create chatroom text file_exists\
+            $filename = "chat/private/$serverId.txt";
+            //error_log($filename);
+            // fopen("chat.txt","w");
+            // fclose("chat.txt");
+            $openfile = fopen($filename,"w") or die("can't open file");
+            //fwrite($openfile,"");
+            fclose($openfile);
+        }
+?>
