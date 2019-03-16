cf1fe507eecacb189f544841ed639ecf3fbe69d9
Nick Peterson <npeterson1996@gmail.com>
2019-03-15 19:06:28 -0500

    update alert

--- chat_engine.js
+++ chat_engine.js
@@ -99,7 +99,7 @@ function updateChat(serverID){
                             data.text[i] = generateMsg(data.text[i], str[0], str[1]);
                             console.log(data.text[i]);
                             //DELETE THIS IF NOT WORKING
-                            alert(allusers);
+                            
                              if(userList(str[0], allusers)){
                                newstr = "<li>"+str[0]+"<li>";
                                allusers.push(str[0]);
