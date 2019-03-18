1502becaecd1933106b2a46874cf4441d874f04f
Student <kauerinspace@gmail.com>
2019-03-18 12:22:20 -0500

    test

--- API/lastMessage.php
+++ API/lastMessage.php
@@ -2,7 +2,7 @@
 //$privateserver = "../chat/private/".$serverId.".txt";
 $server = "9";
 $privateserver = "../chat/private/".$server.".txt";
-  
+$exists = file_exists($privateserver);
 
-  echo json_encode($privateserver);
+  echo json_encode($exists);
 ?>
