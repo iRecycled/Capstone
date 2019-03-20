40c23a09c2beded4e600c428d874fc1754f20e91
Peterson <petersonn3916@uwstout.edu>
2019-03-20 12:15:45 -0500

    test

--- isOnline.php
+++ isOnline.php
@@ -6,7 +6,7 @@
                 http_response_code(500);
                 die('{ "errMessage": "Failed to Connect to DB." }');
             }
-            $username = $_POST['username'];
+            $serverID = $_POST['serverID'];
             //returns usernames of online users
             //$query = "SELECT isOnline FROM Online JOIN WebUser ON Online.UserID = WebUser.UserID WHERE UserName = '$username';";
             $query = "SELECT UserName FROM WebUser JOIN Online ON Online.UserID = WebUser.UserID JOIN ServerMember ON ServerMember.UserID = WebUser.UserID WHERE isOnline = true AND ServerID = '$serverID';"
