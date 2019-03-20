6f5c1f5566d2d0ce678f71703c7754793e061d8c
Peterson <petersonn3916@uwstout.edu>
2019-03-20 11:31:31 -0500

    online user

---
+++ isOnline.php
@@ -0,0 +1,21 @@
+<?php
+/*SELECT isOnline FROM Online JOIN WebUser ON Online.UserID = WebUser.UserID WHERE UserName = "Nick"*/
+        include "database.php";
+        $db = connectToDatabase(DBDeets::DB_NAME);
+        if ($db->connect_error) {
+                http_response_code(500);
+                die('{ "errMessage": "Failed to Connect to DB." }');
+            }
+            $username = $_POST['username'];
+            //returns usernames of online users
+            //$query = "SELECT isOnline FROM Online JOIN WebUser ON Online.UserID = WebUser.UserID WHERE UserName = '$username';";
+            $query = "SELECT UserName FROM WebUser JOIN Online ON Online.UserID = WebUser.UserID JOIN ServerMember ON ServerMember.userID = WebUser.UserID WHERE isOnline = true AND serverID = '$serverID';"
+            //returns array of users that are on that server and are online
+            $result = $db->query($query);
+            //output query result to json array
+            $response = array();
+            while($row = $result->fetch_array(MYSQLI_ASSOC)) {
+                $response[] = $row;
+            }
+            echo json_encode($response);
+?>
