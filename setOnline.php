5f2ee9d239ed96bdb8007e42d3163c496becf563
Nick Peterson <npeterson1996@gmail.com>
2019-04-01 18:05:54 -0500

    query

--- setOnline.php
+++ setOnline.php
@@ -26,9 +26,7 @@
             
             //if the user is not already seen as online, set value to 1 to set online
             if($alreadyExists == false){
-                
-                $query = "UPDATE INTO Online VALUES ($UserID, $serverID, 0);";
-                //$query = "INSERT INTO Online VALUES ($UserID, $serverID, 0);";
+                $query = "INSERT INTO Online VALUES ($UserID, $serverID, 0);";
                 $stmt = simpleQuery($db,$query); //Query prepare failed (1064)
             }
             //if user IS online update the database for user to 1
