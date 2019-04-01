f502d3a25fbcccef99578b813e4a7e14fd7dc1e7
Nick Peterson <npeterson1996@gmail.com>
2019-04-01 17:29:43 -0500

    inside while loop

--- setOnline.php
+++ setOnline.php
@@ -18,10 +18,11 @@
             
             $alreadyExists = false;
              while($row = $result->fetch_array(MYSQLI_ASSOC)) {
-                 $alreadyExists = true;
+                echo("Hello");
+                return;
+                $alreadyExists = true;
              }
-            // echo("Hello");
-            // return;
+            
             
             //if the user is not already seen as online, set value to 1 to set online
             if($alreadyExists == false){
