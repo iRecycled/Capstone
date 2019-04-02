3b756dc3f2bef03c61c10134dd4fc05777b6bd84
Nick Peterson <npeterson1996@gmail.com>
2019-04-01 20:31:41 -0500

    update query

--- setOnline.php
+++ setOnline.php
@@ -14,24 +14,16 @@
             $stmt->fetch();
            
             $query = "SELECT * FROM Online JOIN WebUser ON WebUser.UserID = Online.UserID WHERE username = $username AND serverID = $serverID;";
-            $result = $db->query($query);
-            $alreadyExists = false;
-            //   while($row = $result->fetch_array(MYSQLI_ASSOC)) {
-            //      $alreadyExists = True;
-                 //return $alreadyExists;
-                //  echo("Why tho?");
-                //  return;
-             //}
-            
-            
-            //if the user is not already seen as online, set value to 1 to set online
-            if($alreadyExists == false){
-                $query = "INSERT DISTINCT INTO Online VALUES ($userID, $serverID, 1);";
-                $stmt = simpleQuery($db,$query);
-            }
-            //if user IS online update the database for user to 1
-            else{
+            if(mysql_num_rows($query) != 0){
+                //user already online
                 $query = "UPDATE Online SET isOnline = 1 WHERE UserID = $userID AND ServerID = $serverID;";
                 $stmt = simpleQuery($db,$query);
             }
+            else {
+                //user is NOT online
+                $query = "INSERT INTO Online VALUES ($userID, $serverID, 1);";
+                $stmt = simpleQuery($db,$query);
+            }
+            //$result = $db->query($query);
+            
 ?>
