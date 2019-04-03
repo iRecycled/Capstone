2eadda6b36eea2a6ea7a7ec0ae3dcb32c60bb925
Peterson <petersonn3916@uwstout.edu>
2019-04-03 12:56:44 -0500

    set offline to 3 minutes

--- isOnline.php
+++ isOnline.php
@@ -7,7 +7,7 @@
             }
             $serverID = $_POST['serverID'];
             //returns serverID of online users
-            $query = "SELECT UserName FROM WebUser JOIN Online ON Online.UserID = WebUser.UserID WHERE isOnline = true AND ServerID = '$serverID' AND TIMESTAMPDIFF(MINUTE, timeStamp, NOW()) < 10;";
+            $query = "SELECT UserName FROM WebUser JOIN Online ON Online.UserID = WebUser.UserID WHERE isOnline = true AND ServerID = '$serverID' AND TIMESTAMPDIFF(MINUTE, timeStamp, NOW()) < 3;";
             //returns array of users that are on that server and are online
             $result = $db->query($query);
             //output query result to json array
