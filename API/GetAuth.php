fe047f673755feeea56cf29f676b1cc16d8f236c
emiller7231 <34141292+emiller7231@users.noreply.github.com>
2019-04-03 12:00:05 -0500

    Update GetAuth.php
    Include JQuery this time

--- API/GetAuth.php
+++ API/GetAuth.php
@@ -3,8 +3,10 @@
 <body>
   <h1 id="token"> </h1>
 </body>
+<script src="jquery-3.3.1.min.js"></script>
 <script>
   var TMP = localStorage.getItem('username');
+
   $.ajax({
       url: 'TokenGen.php',
       type: 'POST',
