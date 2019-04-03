7ba7d7947cb5b83920f8877868676699f8791420
Peterson <petersonn3916@uwstout.edu>
2019-04-03 12:27:58 -0500

    timestamp

--- setOnline.php
+++ setOnline.php
@@ -24,9 +24,11 @@
             if($alreadyExists == true){
                 $query = "UPDATE Online SET isOnline = 1 WHERE UserID = $userID AND ServerID = $serverID;";
                 $stmt = simpleQuery($db,$query);
+                $query = mysql_query("UPDATE Online SET timeStamp = NOW() WHERE UserId = $userID AND ServerID = $serverID;";)
+                $stmt = simpleQuery($db, $query);
             }
             else {
-                $query = "INSERT INTO Online VALUES ($userID, $serverID, 1);";
+                $query = "INSERT INTO Online VALUES ($userID, $serverID, 1, NOW());";
                 $stmt = simpleQuery($db,$query);
             }
 ?>
