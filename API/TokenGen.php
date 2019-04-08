3a87900b6498ca744c43c37e4456be64f107c669
emiller7231 <34141292+emiller7231@users.noreply.github.com>
2019-04-08 11:50:36 -0500

    Hopefully works now
    Probs doesnt

--- API/TokenGen.php
+++ API/TokenGen.php
@@ -6,11 +6,10 @@ include "database.php";
             http_response_code(500);
             die('{ "errMessage": "Failed to Connect to DB." }');
         }
-  $data = json_decode("php://input");
-  $user = $data->$username;
+  $user = $_POST['username'];
   $TOK = password_hash(microtime(),PASSWORD_DEFAULT);
   $query = "UPDATE WebUser SET Token='$TOK' WHERE UserName = '$user';";
   $stmt = simpleQuery($db, $query);
   //Update Token FROM WebUser where UserName == $user
-  echo json_encode( $user );
+  echo json_encode( $TOK );
  ?>
