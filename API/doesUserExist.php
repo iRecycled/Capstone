8984044eabcb71f56d979ae80e8a965e197e0b67
Kauer <kauera6146@uwstout.edu>
2019-04-08 19:54:34 -0500

    Test

---
+++ API/doesUserExist.php
@@ -0,0 +1,30 @@
+<?php
+		include "database.php";
+        $db = connectToDatabase(DBDeets::DB_NAME);
+        //connects to database
+        if ($db->connect_error) {
+            http_response_code(500);
+            die('{ "errMessage": "Failed to Connect to DB." }');
+        }
+  //$username = $_POST['username']; 
+  $data = json_decode(file_get_contents("php://input"));
+  $username = $data->username;
+
+
+
+  $query = "SELECT UserID FROM WebUser WHERE UserName = '$username';";
+  //runs query
+        $stmt = simpleQuery($db, $query);
+  //binds results of query to the database
+    $stmt->bind_result($userID);
+    $stmt->fetch();
+    //sends the information from the database back as a json object to the ajax call
+//echo json_encode($username);
+mysql_close($db);
+if($userID==NULL){
+    echo json_encode("Exists");
+}
+else{
+    echo json_encode("Does Not Exist"); 
+}
+?>
