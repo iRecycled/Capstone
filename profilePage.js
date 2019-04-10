9f21b9c557db4a3009415638cfb6530c84691881
Ryan Vanden Boomen <theonlyboomer@gmail.com>
2019-04-10 11:41:24 -0500

    readme added

--- profilePage.js
+++ profilePage.js
@@ -176,8 +176,10 @@ $(document).ready(function(){
 function InsertAvatar(e){
     //sterilize url
     url = e.value.trim()
+    var image = new Image();
+    image.src = url
     //check if url is valid image
-    if(url.substring(url.length-4).valueOf() == '.jpg' || url.substring(url.length-4).valueOf() == '.png' || url.substring(url.length-4).valueOf() == '.gif'){
+    if(image.complete && (url.substring(url.length-4).valueOf() == '.jpg' || url.substring(url.length-4).valueOf() == '.png' || url.substring(url.length-4).valueOf() == '.gif')){
         //run database change query
         $.ajax({
             type: "post",
@@ -194,6 +196,13 @@ function InsertAvatar(e){
         })
     }
 }
+
+function imageExists(url, callback) {
+    var img = new Image();
+    img.onload = function() { callback(true); };
+    img.onerror = function() { callback(false); };
+    img.src = url;
+  }
 
 //adds friend request if valid user
 var isNotFriend = false;
