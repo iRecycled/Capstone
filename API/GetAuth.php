f5e31cef241f17b98092dc3f4431953670201157
Peterson <petersonn3916@uwstout.edu>
2019-04-10 12:31:47 -0500

    value

--- API/GetAuth.php
+++ API/GetAuth.php
@@ -12,7 +12,7 @@
       data: {username: localStorage.getItem('username')},
       success: function(token) {
         document.getElementById("tok").innerHTML = token;
-        document.getElementById("APItoken").innerHTML = token;
+        document.getElementById("APItoken").value = token;
       }
   });
 </script>
